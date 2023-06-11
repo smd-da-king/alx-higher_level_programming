4f2b59857490e3df53fdfdaf8f8221bd2f56d017
smd-da-king <toyin_ng@yahoo.com>
2023-06-11 07:26:13 -0700

    a

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if not my_list:
+        return None
+    res = []
+    for n in my_list:
+        if n % 2 == 0:
+            res.append(True)
+        else:
+            res.append(False)
+    return res
