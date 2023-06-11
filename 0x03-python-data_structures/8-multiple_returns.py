191da79a8a3aa8981cbf04ea2ba499dfef231e34
smd-da-king <toyin_ng@yahoo.com>
2023-06-11 07:16:01 -0700

    a

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    str_len = len(sentence)
+    if str_len == 0:
+        return (str_len, None)
+    else:
+        return (str_len, sentence[0])
