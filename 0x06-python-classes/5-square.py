fa5cb58fd0ea902bfe629548eed4e7d3d9cfede4
smd-da-king <toyin_ng@yahoo.com>
2023-06-27 21:03:15 -0700

    a

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""
+This a square module.
+"""
+
+
+class Square:
+    """
+    represents a Square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initialize a Square.
+
+        Args:
+            length (int): size of the square.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """
+        the area of the square.
+
+        Returns:
+            the square area.
+        """
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """
+        get the size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        set the size of the square.
+
+        Args:
+            value (int): size value.
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+
+    def my_print(self):
+        """
+        print in stdout the square.
+        """
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
+
+        if self.__size == 0:
+            print("")
