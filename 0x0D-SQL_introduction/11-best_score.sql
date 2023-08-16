0b74795be1baabbdf8cba9fa320403167fb6d1f8
smd-da-king <toyin_ng@yahoo.com>
2023-08-16 03:00:54 -0700

    a

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,4 @@
+-- >= 10 values in a table
+SELECT score, name FROM second_table
+WHERE score >= 10
+ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- update a value in a table
+UPDATE second_table SET score=10 WHERE name="Bob";
