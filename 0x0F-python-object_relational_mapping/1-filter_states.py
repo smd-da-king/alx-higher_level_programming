4a58e222d3f282b9ad8cc1ed84b1b96c83f6977f
smd-da-king <toyin_ng@yahoo.com>
2023-09-17 01:01:56 -0700

    a

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""List names that are starting with N."""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id"
+    cur.execute(query)
+
+    for row in cur.fetchall():
+        print(row)
+
+    cur.close()
+    db.close()
